1f7ea8eaaf00de5e266f3786152fb4223538bb5a
Bernadette St.George <bernadette.stgeorge@gmail.com>
2019-08-08 09:56:59 -0700

    Server responds to GET requests with movie data

--- server/app.js
+++ server/app.js
@@ -1,8 +1,43 @@
 const express = require('express');
-
+const axios = require('axios');
+const morgan = require('morgan');
 
 const app = express();
 
-// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+let cache = {};
+
+app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+    if (req.query.i){
+        if (cache.hasOwnProperty(req.query.i)) {
+            res.send(cache[req.query.i]);
+        } else {
+            axios.get(`http://www.omdbapi.com/?i=${req.query.i}&apikey=8730e0e`)
+            .then(function(response) {
+                cache[req.query.i] = response.data;
+                res.send(response.data);
+            })
+            .catch(function(error) {
+                console.log(error);
+            })
+        }
+    } else { 
+        req.query.t = req.query.t.replace(" ", "%20");
+        if (cache.hasOwnProperty(req.query.t)) {
+            res.send(cache[req.query.t]);
+        } else {
+            axios.get(`http://www.omdbapi.com/?t=${req.query.t}&apikey=8730e0e`)
+            .then((response) => {
+                cache[req.query.t] = response.data;
+                res.send(response.data);
+            })
+            .catch(function(error) {
+                console.log(error);
+            })
+        }
+    }
+})
+
 
 module.exports = app;
